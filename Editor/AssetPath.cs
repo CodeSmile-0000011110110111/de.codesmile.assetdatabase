98d9ecc1eb048e603c2a3a8f0265e8e95058dbb0
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2023-11-01 15:31:56 +0100

    assetpath doc changes

--- Editor/AssetPath.cs
+++ Editor/AssetPath.cs
@@ -8,6 +8,14 @@ using Object = System.Object;
 
 namespace CodeSmile.Editor
 {
+	/// <summary>
+	///     Represents a relative path to an asset file in the project.
+	///		Instances are implicitly and explicitly convertible to/from string.
+	///     All path separators are forward slashes for compatibility with Windows, Mac, Linux.
+	///     Leading and trailing path separators are trimmed.
+	///     Instances can be initialized with a relative or full (absolute) path, or a relative/full path
+	///     to a directory plus a filename and optional extension (default: 'asset').
+	/// </summary>
 	public class AssetPath : IEquatable<AssetPath>, IEquatable<String>
 	{
 		public const String DefaultExtension = "asset";
@@ -15,17 +23,17 @@ namespace CodeSmile.Editor
 		private readonly String m_RelativePath = String.Empty;
 
 		/// <summary>
-		///     Creates and returns the full path with forward slashes as separators.
+		///     Creates and returns the full path, with forward slashes as separators.
 		/// </summary>
 		public String FullPath => Path.GetFullPath(m_RelativePath).NormalizePathSeparators();
 
 		/// <summary>
-		///     Returns the relative path. Same as ToString().
+		///     Implicit conversion to string (relative asset path). Same as ToString().
 		/// </summary>
 		public static implicit operator String(AssetPath assetPath) => assetPath.m_RelativePath;
 
 		/// <summary>
-		///     Creates and returns an AssetPath instance from a string.
+		///     Explicit creation of an AssetPath instance initialized with a string path (full or relative).
 		/// </summary>
 		/// <param name="fullOrRelativePath"></param>
 		/// <returns></returns>
@@ -50,7 +58,7 @@ namespace CodeSmile.Editor
 
 		public static Boolean operator !=(AssetPath ap1, Object other) => !(ap1 == other);
 
-		public static Boolean operator ==(Object other, AssetPath ap2) => (ap2 == other);
+		public static Boolean operator ==(Object other, AssetPath ap2) => ap2 == other;
 
 		public static Boolean operator !=(Object other, AssetPath ap2) => !(ap2 == other);
 
@@ -72,7 +80,7 @@ namespace CodeSmile.Editor
 			// it must be followed by a path separator, eg "Assets/.."
 			normalizedPath = normalizedPath.TrimStart('/');
 			var startsWithAssets = normalizedPath.ToLower().StartsWith("assets");
-			return startsWithAssets && (normalizedPath.Length <= 6 || IsPathSeparator(normalizedPath[6]));
+			return startsWithAssets && (normalizedPath.Length <= 6 || normalizedPath[6].Equals('/'));
 		}
 
 		private static void ThrowIfNotAProjectPath(String normalizedPath, String fullPath)
@@ -87,29 +95,32 @@ namespace CodeSmile.Editor
 
 		private static void ThrowIfNullOrWhitespace(String param, String paramName)
 		{
-#if DEBUG
 			if (param == null)
 				throw new ArgumentNullException($"{paramName} is null");
 			if (String.IsNullOrWhiteSpace(param))
 				throw new ArgumentException($"{paramName} is empty or whitespace");
-#endif
 		}
 
-		private static Boolean IsPathSeparator(Char c) => c.Equals('/') || c.Equals('\\');
-
-		public AssetPath() : this(Application.dataPath) {}
+		private static void ThrowIfContainsPathSeparators(String fileName, String paramName)
+		{
+			var normalized = fileName.NormalizePathSeparators();
+			if (normalized.Contains('/'))
+				throw new ArgumentException("filename contains path separators", paramName);
+		}
 
 		/// <summary>
-		///     Normalizes a path to forward slashes and makes the path relative so it starts with 'Assets'.
-		///     Also trims any leading and trailing slashes.
-		///     Example input path: 'C:\Users\Urso Clever\Untiy Projects\First Porject\Assets\something'
-		///     Asset path: 'Assets/something'
-		///     Notes: Input path is required to point to the project's root or below.
-		///     Path separators are normalized to forward slashes.
+		///     Creates an asset path pointing to the 'Assets' root folder.
+		/// </summary>
+		public AssetPath() : this("Assets") {}
+
+		/// <summary>
+		///     Creates an asset path from either a full or relative path.
+		///     Example input path: 'C:\Users\Urso Clever\Untiy Projects\First Projcet\Assets\something'
+		///     Resulting AssetPath: 'Assets/something'
 		/// </summary>
 		/// <param name="fullOrRelativePath">Absolute path to project's root folder or a subfolder.</param>
 		/// <exception cref="ArgumentException">
-		///     If path does not point to the current project's root folder or one of its subfolders.
+		///     If path does not start with the project's Assets path (Application.dataPath).
 		/// </exception>
 		/// <returns></returns>
 		public AssetPath(String fullOrRelativePath)
@@ -119,20 +130,21 @@ namespace CodeSmile.Editor
 		}
 
 		/// <summary>
-		///     Combines a path to a folder in the project with a filename and extension to form a valid path string
-		///     to an asset file.
-		///     Note: Path separators get normalized and trimmed, for example these are valid input paths:
-		///     'Assets\\folder'
-		///     '\\Assets/folder'
-		///     '/Assets/folder\\'
+		///     Creates an asset path from either a full or relative path to a directory, a filename,
+		///     and an optional extension (default: 'asset').
+		///     Example input: '\Assets\Some/Sub\Dir' - 'New File' - 'MyExt'
+		///     Resulting AssetPath: 'Assets/Some/Sub/Dir/New File.myext'
 		/// </summary>
-		/// <param name="directory">Path to a folder for the file. Can be a relative or absolute path.</param>
-		/// <param name="fileName">Name of the file without extension.</param>
-		/// <param name="extension">Extension of the file (default: 'asset'). Leading dots will be trimmed.</param>
-		/// <returns>
-		///     The relative path to the asset file. For example if input is 'Assets/folder', 'file'
-		///     and 'ext' the returned path will be 'Assets/folder/file.ext'
-		/// </returns>
+		/// <param name="directory">
+		///     Path to a directory under Assets, or just 'Assets'. Can be a relative or absolute path.
+		///     Leading/trailing path separators are trimmed.
+		/// </param>
+		/// <param name="fileName">Name of the file without extension. Must not contain path separators.</param>
+		/// <param name="extension">
+		///     Extension of the file (default: 'asset'). Leading dots will be trimmed. Extension will be
+		///     lowercase.
+		/// </param>
+		/// <returns>The relative path to the asset file. </returns>
 		/// <exception cref="ArgumentException"></exception>
 		public AssetPath(String directory, String fileName, String extension = DefaultExtension)
 		{
@@ -142,7 +154,7 @@ namespace CodeSmile.Editor
 			ThrowIfContainsPathSeparators(fileName, nameof(fileName));
 
 			var relativeDir = ToRelative(directory.NormalizePathSeparators());
-			m_RelativePath = $"{relativeDir}/{fileName}.{extension.TrimStart('.')}";
+			m_RelativePath = $"{relativeDir}/{fileName}.{extension.TrimStart('.').ToLower()}";
 		}
 
 		public Boolean Equals(AssetPath other)
@@ -159,13 +171,6 @@ namespace CodeSmile.Editor
 		public override Boolean Equals(Object obj) => Equals(obj as AssetPath);
 
 		public override Int32 GetHashCode() => m_RelativePath.GetHashCode();
-
-		private void ThrowIfContainsPathSeparators(String fileName, String paramName)
-		{
-			var normalized = fileName.NormalizePathSeparators();
-			if (normalized.Contains('/'))
-				throw new ArgumentException("filename contains path separators", paramName);
-		}
 
 		/// <summary>
 		///     Returns the relative path as string. Same as implicit string conversion.
