b9045b2088faf1b1e302edc931cc928cfb813576
CodeSmile-0000011110110111 <CodeSmile-0000011110110111@users.noreply.github.com>
2024-01-24 18:03:27 +0000

    deploy: 546acc36ee728bb44b80fecefc41d3d9f45bdc5b

--- html/search/all_4.js
+++ html/search/all_4.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['4_200_20nov_2024_202023_0',['v1.4.0 - Nov 24, 2023',['../md__p_1_2de_8codesmile_8assetdatabase_2_c_h_a_n_g_e_l_o_g.html#v140---nov-24-2023',1,'']]],
   ['4_201_20nov_2024_202023_1',['v1.4.1 - Nov 24, 2023',['../md__p_1_2de_8codesmile_8assetdatabase_2_c_h_a_n_g_e_l_o_g.html#v141---nov-24-2023',1,'']]],
-  ['4_20nov_2013_202023_2',['v1.1.4 - Nov 13, 2023',['../md__p_1_2de_8codesmile_8assetdatabase_2_c_h_a_n_g_e_l_o_g.html#v114---nov-13-2023',1,'']]]
+  ['4_20jan_2024_202024_2',['v1.8.4 - Jan 24, 2024',['../md__p_1_2de_8codesmile_8assetdatabase_2_c_h_a_n_g_e_l_o_g.html#v184---jan-24-2024',1,'']]],
+  ['4_20nov_2013_202023_3',['v1.1.4 - Nov 13, 2023',['../md__p_1_2de_8codesmile_8assetdatabase_2_c_h_a_n_g_e_l_o_g.html#v114---nov-13-2023',1,'']]]
 ];
